b02f3c9ef92a92a36b8ed7a9d9cdd888c0eca5f2
ElJeringas <santiagojss32@gmail.com>
2023-08-15 20:25:08 -0500

    browse page

--- item/views.py
+++ item/views.py
@@ -8,8 +8,12 @@ from .forms import NewItemForm, EditItemForm
 
 def items(request):
     query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
     categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
 
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
@@ -18,6 +22,7 @@ def items(request):
         'items':items,
         'query':query,
         'categories':categories,
+        'category_id':int(category_id),
     })
 
 def detail (request,pk):
